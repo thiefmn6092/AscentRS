f24e5c178390098d2504504d8c865da9245d0662
thiefmn6092 <thiefmn6092@gmail.com>
2012-06-19 03:21:21 -0400

    Begun development.

---
+++ src/net/thiefmn6092/ascent/Ascent.java
@@ -0,0 +1,18 @@
+package net.thiefmn6092.ascent;
+
+/**
+ * The main class.
+ * @author thiefmn6092
+ *
+ */
+public class Ascent {
+
+	/**
+	 * The JVM entry point.
+	 * @param args The command-line arguments.
+	 */
+	public static void main(String[] args) {
+		// TODO
+	}
+
+}
